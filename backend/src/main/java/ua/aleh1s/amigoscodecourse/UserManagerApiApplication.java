22605b71271e70971426bb290921d0f77e5fdff6
Oleksandr Palamarchuk <sashap740@gmail.com>
2023-07-07 15:33:46 +0300

    update names

--- backend/src/main/java/ua/aleh1s/amigoscodecourse/UserManagerApiApplication.java
+++ backend/src/main/java/ua/aleh1s/amigoscodecourse/UserManagerApiApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AmigoscodeCourseApplication {
+public class UserManagerApiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AmigoscodeCourseApplication.class, args);
+        SpringApplication.run(UserManagerApiApplication.class, args);
     }
 }
--- backend/src/main/java/ua/aleh1s/amigoscodecourse/customer/CustomerRepository.java
+++ backend/src/main/java/ua/aleh1s/amigoscodecourse/customer/CustomerRepository.java
@@ -3,8 +3,6 @@ package ua.aleh1s.amigoscodecourse.customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     boolean existsCustomerById(Integer id);
